4a327004393059f89517041025d1b1ad52816fbc
ELijah Murray <elijahmurray@gmail.com>
2016-09-10 11:47:22 -0700

    set up basic validations

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,8 @@
 class Relationship < ActiveRecord::Base
   REMINDER_FREQUENCIES = ["Once a Week", "Once a Month", "Once a Year"]
   RELATIONSHIP_TYPES = ["Family", "Professional Network", "Mentor", "Mentee"]
+
+  validates :name, presence: true
+  validates :relationship_type, presence: true
+  validates :reminder_frequency, presence: true
 end
