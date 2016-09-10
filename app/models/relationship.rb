478888ca3640eaaf735675db85e51c4b310d3f28
ELijah Murray <elijahmurray@gmail.com>
2016-09-10 11:29:29 -0700

    update the attributes for the relationship

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,4 @@
 class Relationship < ActiveRecord::Base
+  REMINDER_FREQUENCIES = ["Once a Week", "Once a Month", "Once a Year"]
+  RELATIONSHIP_TYPES = ["Family", "Professional Network", "Mentor", "Mentee"]
 end
