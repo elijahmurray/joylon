e909b2b2a44dd2ba2cd7ce8d7370b74bf3a9c038
Elijah Murray <elijahmurray@Elijahs-MacBook.local>
2016-09-10 09:54:13 -0700

    setup model

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,5 @@
+class RelationshipsController < ApplicationController
+  def index
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150208023351) do
+ActiveRecord::Schema.define(version: 20160910164453) do
+
+  create_table "relationships", force: :cascade do |t|
+    t.string   "name"
+    t.string   "type"
+    t.string   "reminder_frequency"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
