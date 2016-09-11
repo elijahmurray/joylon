f27dd07231247e057f0aeb161a7d354ebc11dfb1
ELijah Murray <elijahmurray@gmail.com>
2016-09-10 17:59:42 -0700

    prepping for notifications

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -9,4 +9,16 @@ class Relationship < ActiveRecord::Base
   def sorted_reminders
     self.reminders.sort{|r| r.status <=> r.status}
   end
+
+  def reminder_notifications
+    self.reminders.select{|r| r.status == 'late'}
+  end
+
+  def update_all_reminder_statuses
+    #if reminder.due_date < Date.today() - 1
+    #  send notification
+    #  else
+    #  return
+    #  end
+  end
 end
