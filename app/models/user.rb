82bfa2b415fc26b9f5c623860388ee667375ce24
ELijah Murray <elijahmurray@gmail.com>
2016-09-11 18:36:54 -0700

    test emails coming through

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   def reminders_email
-    reminders = []
-    self.relationships.select do |rel| 
-      reminders << rel.reminders_by_status('late')
+    late_reminders = {}
+    upcoming_reminders = {}
+    self.relationships.each do |rel| 
+      late_reminders[rel.name] = rel.reminders_by_status('late')
+      upcoming_reminders[rel.name] =  rel.reminders_by_status('active')
     end
 
     email_address = self.email
@@ -19,7 +21,7 @@ class User < ActiveRecord::Base
       from 'elijahmurray@gmail.com'
       subject 'Your Daily Joylon Reminders'
       html_part do
-        body ApplicationController.new.render_to_string('emails/_daily_reminders', locals: {reminders: reminders}, :layout => false)
+        body ApplicationController.new.render_to_string('emails/_daily_reminders', locals: {upcoming_reminders: upcoming_reminders, late_reminders: late_reminders}, :layout => false)
       end
     end
   end
